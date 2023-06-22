a6e97b9b30f9f6b4fa8baf57a9a698bd5fb348ef
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-22 22:05:24 +0530

    Update MinNumberOfPlatforms.cpp

--- Day08/MinNumberOfPlatforms.cpp
+++ Day08/MinNumberOfPlatforms.cpp
@@ -1,14 +1,18 @@
-int calculateMinPatforms(int at[], int dt[], int n) {
+int calculateMinPatforms(int at[], int dt[], int n)
+{
     // Write your code here.
-    sort(at,at+n);
-    sort(dt,dt+n);
-    int i = 0 , j = 0;
+    sort(at, at + n);
+    sort(dt, dt + n);
+    int i = 0, j = 0;
     int maxi = 0;
     int platforms = 0;
-    while(i < n && j < n){
-        if(at[i] <= dt[j]) i++ , platforms++;
-        else j++ , platforms--;
-        maxi = max(platforms,maxi);
+    while (i < n && j < n)
+    {
+        if (at[i] <= dt[j])
+            i++, platforms++;
+        else
+            j++, platforms--;
+        maxi = max(platforms, maxi);
     }
     return maxi;
 }
