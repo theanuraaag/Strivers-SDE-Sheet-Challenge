3da2541ff3df155bae48c95aff838177a2f42eb8
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-26 22:07:31 +0530

    Update PairSum.cpp

--- Day04/PairSum.cpp
+++ Day04/PairSum.cpp
@@ -2,22 +2,35 @@
 
 vector<vector<int>> pairSum(vector<int> &arr, int s)
 {
+   sort(arr.begin(), arr.end());
+
    vector<vector<int>> ans;
 
-   for(int i=0;i<arr.size();i++)
+   for (int i = 0; i < arr.size() - 1; i++)
    {
-      for(int j=0;j<arr.size();j++)
+
+      vector<int> temp;
+
+      for (int j = i + 1; j < arr.size(); j++)
       {
-         if(arr[i]+arr[j]==s)
+
+         if (arr[i] + arr[j] == s)
          {
-            vector<int> v;
-            v.push_back(min(arr[i],arr[j]));
-            v.push_back(max(arr[i],arr[j]));
-            ans.push_back(v);
-            arr[j]=100001;
+
+            temp.push_back(arr[i]);
+
+            temp.push_back(arr[j]);
+
+            ans.push_back(temp);
+         }
+
+         if (arr[i] + arr[j] > s)
+         {
+
+            break;
          }
       }
    }
-   sort(ans.begin(),ans.end());
-   return(ans);
+
+   return ans;
 }
