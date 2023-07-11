95c38468c7d8fabdc0dfaf033438d24ba43037ed
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-07-11 22:19:09 +0530

    Create ATOI.cpp

---
+++ Day15/ATOI.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+int atoi(string s)
+{
+
+    // Write your code here.
+
+    bool flag = true;
+
+    int digit = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+
+        int rem = (int)s[i] - 48;
+
+        if (rem == -3)
+        {
+
+            flag = false;
+        }
+
+        else if (rem >= 0 && rem <= 9)
+        {
+
+            digit = digit * 10 + rem;
+        }
+
+        else
+        {
+            continue;
+        }
+    }
+
+    if (flag == false)
+
+        digit = -1 * digit;
+
+    return digit;
+}
